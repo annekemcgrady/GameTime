137c1e86163d8c0c927dee1b0eef0beab1644653
Emily Dittmer <emilyedittmer@gmail.com>
2019-05-23 14:03:41 -0600

    Display player names on DOM

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,13 +6,18 @@ import Game from './Game';
 
 const domUpdates = {
 
+  displayNames: function(user1, user2) {
+    $('.name-player-one').html(user1);
+    $('.name-player-two').html(user2);
+  }
+
   // displayWords: function(string){
   //   $('#words').text(string)
   // }
 
-  displayScore: function(score){
-    $('.player-one-score').text(score)
-  }
+  // displayScore: function(score){
+  //   $('.player-one-score').text(score)
+  // }
 
 
 };
